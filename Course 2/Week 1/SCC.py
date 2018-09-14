7c34f356793f6b9456bdd5eaa09cb682a7ab53ae
Christoph Meier <chris_m216@hotmail.com>
2018-09-13 23:30:08 -0500

    updated terminal output for SCC.py

--- Course 2/Week 1/SCC.py
+++ Course 2/Week 1/SCC.py
@@ -30,7 +30,7 @@ def DFS(graph, i, book_keeping):
     book_keeping.finishing_times[book_keeping.t] = i
 
 def create_graphs(file_name):
-    print("Creating graphs...")
+    print("Creating graphs...", end=" ", flush=True)
     graph, graph_rev = defaultdict(set), defaultdict(set)
     this_folder = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(this_folder, file_name)
@@ -39,17 +39,20 @@ def create_graphs(file_name):
             num1, num2 = map(int, line.strip().split(" "))
             graph[num1].add(num2)
             graph_rev[num2].add(num1)
+    print("Done.")
     return graph, graph_rev
 
 def SCCs(graph, graph_rev):
     book_keeping1 = Book_keeping()
     book_keeping2 = Book_keeping()
 
-    print("Running DFS on reverse graph...")
+    print("Running DFS on reverse graph...", end=" ", flush=True)
     DFS_loop(graph_rev, book_keeping1)
+    print("Done.")
 
-    print("Running DFS on forward graph...")
+    print("Running DFS on forward graph...", end=" ", flush=True)
     DFS_loop(graph, book_keeping2, order_dict=book_keeping1.finishing_times)
+    print("Done.")
 
     scc = dict(Counter(book_keeping2.leaders.values()))
     return scc
