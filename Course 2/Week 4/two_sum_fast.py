81f628b8b0e1257507f31e449e68d3998ec14ef9
Christoph Meier <chris_m216@hotmail.com>
2018-09-21 16:05:59 -0500

    faster and simpler two_sum

--- Course 2/Week 4/two_sum_fast.py
+++ Course 2/Week 4/two_sum_fast.py
@@ -1,59 +1,45 @@
 import os
-import threading
 
-def create_list(file_name):
+def create_set(file_name):
     this_folder = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(this_folder, file_name)
     
-    number_list = []
+    number_set = set()
     with open(my_file) as f:
         for line in f:
-            number_list.append((int(line.strip())))
-    return number_list
+            number_set.add((int(line.strip())))
+    return number_set
 
-def binary_search_bound(my_list, number, type=None):
-    """Returns index of number in an ordered list in log n time"""
-    low = 0
-    high = len(my_list) - 1
-    mid = low + ((high - low) // 2)
-    while low <= high:
-        mid = low + ((high - low) // 2)
-        if number == my_list[mid]:
-            return mid
-        else:
-            if number < my_list[mid]:
-                high = mid - 1
-            elif number > my_list[mid]:
-                low = mid + 1
-    if type == "low" and my_list[mid] < number and mid < len(my_list) - 1:
-        mid += 1
-    return mid
+def two_sum(numbers, low, high):
+    ts = set()
+    top_pointer = len(numbers) - 1
+    bot_pointer = len(numbers) - 1
+    x_pointer = 0
+    
+    while x_pointer < bot_pointer:
+    # for x_pointer in range(0, len(numbers)):
+        while numbers[x_pointer] + numbers[bot_pointer - 1] >= low:
+            bot_pointer -= 1
+        
+        while numbers[x_pointer] + numbers[top_pointer] > high and top_pointer > bot_pointer:
+            top_pointer -= 1
 
-def two_sum_mod(alist, x, t_low, t_high):
-    y_low = t_low - x
-    y_high = t_high - x
-    pos_low = binary_search_bound(alist, y_low, type="low")
-    pos_high = binary_search_bound(alist, y_high)
-    ys = [y for y in alist[pos_low:pos_high]]
-    if alist[pos_high] <= y_high:
-        ys.append(y_high)
-    return ys
+        for y in numbers[bot_pointer:top_pointer]:
+            ts.add(numbers[x_pointer] + y)
+
+        if numbers[x_pointer] + numbers[top_pointer] >= low and numbers[x_pointer] + numbers[top_pointer] <= high:
+            ts.add(numbers[x_pointer] + numbers[top_pointer])
+        x_pointer += 1
+
+    return ts
+
 
 if __name__ == "__main__":
-    numbers = create_list("algo1-programming_prob-2sum.txt")
-    numbers = sorted(list(set(numbers)))
+    numbers = sorted(list(create_set("algo1-programming_prob-2sum.txt")))
+
     low = -10000
     high = 10000
 
-    total = set()
-    total_list = []
-    for x in numbers:
-        for y in two_sum_mod(numbers, x, low, high):
-            if x + y not in total:
-                total_list.append((x + y, x, y))
-            total.add(x + y)
-    total_list = sorted(total_list)
-    for t in total_list:
-        print(t[0] >= low and t[0] <= high, t)
+    my_set = two_sum(numbers, low, high)
+    print(len(my_set))
 
-    print(len(total_list))
