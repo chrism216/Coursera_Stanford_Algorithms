a3b023b74c90eea27582fe4781ba854f4a3c3ff8
Christoph Meier <chris_m216@hotmail.com>
2018-09-23 20:03:37 -0500

    small correction for boundary special cases

--- Course 2/Week 4/two_sum_fast.py
+++ Course 2/Week 4/two_sum_fast.py
@@ -3,7 +3,6 @@ import os
 def create_set(file_name):
     this_folder = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(this_folder, file_name)
-    
     number_set = set()
     with open(my_file) as f:
         for line in f:
@@ -15,27 +14,23 @@ def two_sum(numbers, low, high):
     top_pointer = len(numbers) - 1
     bot_pointer = len(numbers) - 1
     x_pointer = 0
-    
     while x_pointer < bot_pointer:
         while numbers[x_pointer] + numbers[bot_pointer - 1] >= low:
             bot_pointer -= 1
-        
-        while numbers[x_pointer] + numbers[top_pointer - 1] >= high:
+
+        while numbers[x_pointer] + numbers[top_pointer - 1] > high:
             top_pointer -= 1
 
         for y in numbers[bot_pointer:top_pointer]:
             ts.add(numbers[x_pointer] + y)
 
         x_pointer += 1
-
     return ts
 
 if __name__ == "__main__":
     numbers = sorted(list(create_set("algo1-programming_prob-2sum.txt")))
-
     low = -10000
     high = 10000
-
     my_set = two_sum(numbers, low, high)
     print(len(my_set))
 
